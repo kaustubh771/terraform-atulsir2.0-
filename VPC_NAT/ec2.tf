405a8b6a369a128d7695e060dee66ed74a315af5
kaustubh771 <kaustubhtyhakare0709@gmail.com>
2022-09-24 19:29:48 +0530

    changes

--- VPC_NAT/ec2.tf
+++ VPC_NAT/ec2.tf
@@ -3,7 +3,7 @@
 resource "aws_security_group" "security_group" {
   name        = "terraform_ec2_private_sg"
   description = "Allow TLS inbound traffic"
-  vpc_id      = "${aws_vpc.devops_vpc.id}"
+  vpc_id      = "$aws_vpc.devops_vpc.id"
 
   ingress {
     from_port        = 22
@@ -38,7 +38,7 @@ resource "aws_security_group" "security_group" {
 }
 
 output "aws_security_gr_id" {
-    value = "${aws_security_group.security_group.id}"
+    value = "aws_security_group.security_group.id"
 }
 
 #Creating EC2 IN public Subnet
@@ -46,8 +46,8 @@ output "aws_security_gr_id" {
 resource "aws_instance" "public_instance" {
     ami           = "ami-017fecd1353bcc96e"
     instance_type = "t2.micro"
-    vpc_security_group_ids = [ "${aws_security_group.security_group.id}" ] 
-    subnet_id = "${aws_subnet.public_subnet.id}"
+    vpc_security_group_ids = "aws_security_group.security_group.id" 
+    subnet_id = "aws_subnet.public_subnet.id"
     key_name = "newkey"
     count = 1
     associate_public_ip_address = true
@@ -60,46 +60,12 @@ resource "aws_instance" "public_instance" {
 resource "aws_instance" "private_instance" {
     ami           = "ami-017fecd1353bcc96e"
     instance_type = "t2.micro"
-    vpc_security_group_ids = [ "${aws_security_group.security_group.id}" ] 
-    subnet_id = "${aws_subnet.private_subnet.id}"
+    vpc_security_group_ids = "aws_security_group.security_group.id" 
+    subnet_id = "aws_subnet.private_subnet.id"
     key_name = "newkey"
     count = 1
     associate_public_ip_address = false
     tags = {
         Name = "private_instance"
     } 
-}
-
-
-
-
-
-
-
-
-
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "HelloWorld"
-  }
 }
