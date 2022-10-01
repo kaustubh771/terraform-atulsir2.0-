65cc79bfd00194a803e6ce287af798d25c4dbae2
kaustubh771 <105920368+kaustubh771@users.noreply.github.com>
2022-10-01 12:32:19 +0530

    Update main.tf

--- 15 buckets/main.tf
+++ 15 buckets/main.tf
@@ -1,8 +1,6 @@
 provider "aws" {
   region     = "us-west-2"
-  access_key = "AKIARP5PLQ5KKBZTVSG7"
-  secret_key = "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"
-}
+  
 
 resource "aws_s3_bucket" "c" {
   count=15
